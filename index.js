bfa96ec712720821e4b7bb0c2452bb7a674a0b5e
mustafa <ma5755333@gmail.com>
2019-07-14 10:40:43 +0300

    Modified

--- index.js
+++ index.js
@@ -11,24 +11,25 @@ function myfunction() {
     }
     var YearOfBirth = prompt("Enter your year of birth");
     var gender = prompt("Enter gender (Male or Female):");
-    var maleName = (" Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
-    var femaleName = ("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
+    var maleName = [" Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    // console.log(maleName)
+    var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var DayOfCentury = parseInt(DateOfBirth.slice(0, 2));
     var DayofBirth = parseInt(YearOfBirth.slice(2, 4));
-    var Dayoftheweek = (((DayOfCentury / 4) - 2 * DayOfCentury - 1) + ((5 * YearOfBirth / 4)) + ((26 * (YearOfBirth + 1) / 10))) % 7;
+    var Dayoftheweek = parseInt((((DayOfCentury / 4) - 2 * DayOfCentury - 1) + ((5 * YearOfBirth / 4)) + ((26 * (YearOfBirth + 1) / 10))) % 7);
+    // console.log(Dayoftheweek)
 
 if(gender === "Male"){
-    var Mname = maleName[Dayoftheweek - 1];
+    var Mname = maleName[Dayoftheweek-1];
     var Day = Days[Dayoftheweek - 1];
     alert("If you were born on"  + Day +  "your name is " + Mname);
 
 } else if(gender === "Female") {
    
         var Fname = femaleName[Dayoftheweek - 1];
-        var Day = Days[ Dayoftheweek - 1];
+        var Day = Days[Dayoftheweek - 1];
         alert("If you were born on"  + Day +  "your name is " + Fname);
     }
 }
 myfunction();
-
