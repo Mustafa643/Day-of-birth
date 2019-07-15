bdaf8c064fa732855c48249a4e3becbbcc4dd67d
mustafa <ma5755333@gmail.com>
2019-07-15 12:06:51 +0300

    Comitted

--- ReadMe.md
+++ ReadMe.md
@@ -3,16 +3,24 @@
 ## It is a modern sysytem that generates Akhan names according to day you were born given the Date of Birth
 
 ### Author
-Mustafa Ahmed
+#### Mustafa Ahmed
 
- ####Setup
+ #### Setup
+ Open this link on a supported web broser to interract with the application: https://mustafa643.github.io/Day-of-birth/ To view the source code :
+ install Atom/Visual studio code
+ git init
+ git clone https://github.com/Mustafa643/Day-of-birth.git
+ Open the folder where the cloned repository is saved
 
- ##### When the user inputs Date,Month and Year the system tells him/her which day of the week they were born and their Akan Names
+
+ #### When the user inputs Date,Month and Year the system tells him/her which day of the week they were born and their Akan Names
 
  ### Technologies used
   ##### HTML
   ##### Css
   ##### JavaScript
   ##### Bootstrap
-  
+
+  # LISENCE
+
 
--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function myfunction() {
-    // d    
+     
     var DateOfBirth = document.getElementById("Date").value;
-    console.log(DateOfBirth);
+   
     
-    // // if(DateOfBirth < 1 || DateOfBirth > 31){
-    // //     alert("invalid date");
-    // }
+    
     var MonthOfBirth = document.getElementById("Month").value;
-    console.log(MonthOfBirth)
-    // if(MonthOfBirth < 1 || MonthOfBirth > 12){
-    //     alert("invalid month");
-    // }
+    
+    
     var YearOfBirth = document.getElementById("Year").value;
-    console.log(YearOfBirth)
-    var gender = document.getElementById("gender").value;
+    
+    var gender1 = document.getElementById("gender1").value;
+    var gender2 = document.getElementById("gender2").value;
+
     var maleName = [" Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    // console.log(maleName)
+    
     var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -24,18 +22,21 @@ function myfunction() {
 
     var DayofBirth = parseInt(YearOfBirth.toString().slice(2, 4));
 
-    var Dayoftheweek = parseInt((((DayOfCentury / 4) - 2 * DayOfCentury - 1) + ((5 * YearOfBirth / 4)) + ((26 * (YearOfBirth + 1) / 10))) % 7);
-    // console.log(Dayoftheweek)
-
-if(gender === "Male"){
+    var Dayoftheweek = parseInt(((DayOfCentury / 4) - 2 * DayOfCentury - 1) + ((5 * YearOfBirth / 4)) + ((26 * (YearOfBirth + 1) / 10))) % 7;
+    
     var Mname = maleName[Dayoftheweek-1];
     var Day = Days[Dayoftheweek - 1];
+
+    var Fname = femaleName[Dayoftheweek - 1];
+    var Day = Days[Dayoftheweek - 1];
+
+if(gender1.checked == true){
+    
     document.getElementById("outputs").innerHTML=("If you were born on "     + Day +    " your name is " + Mname);
 
-} else if(gender === "Female") {
+} else if(gender2.checked == true) {
    
-        var Fname = femaleName[Dayoftheweek - 1];
-        var Day = Days[Dayoftheweek - 1];
+        
         document.getElementById("outputs").innerHTML=("If you were born on "     + Day +    " your name is " + Fname);
     }
     myfunction();
